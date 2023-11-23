301f0b1caad8ae62ab557c0493a33b800cc2e9bf
Dmytro <dimasukgaborak@gmail.com>
2023-11-23 19:22:44 +0200

    Create functions.php

---
+++ php/php_basis/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+# Створення ф-ц. з параметрами
+
+function sum ($a, $b) {
+    return $a + $b;
+}
+
+function subtraction($a, $b) {
+    return $a - $b;
+}
+
+# Виклик ф-ц.
+
+echo sum(12, 13) . '<br>';
+echo subtraction(55, 34);
