7faf70554696e1d484ed469de05a765a02739452
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-10-06 13:50:13 +0300

    Create script.js

---
+++ js/course/practice/script.js
@@ -0,0 +1,11 @@
+let num1 = '2'; 
+
+if (num1 < 0) {
+    console.log('Число від\'ємне');
+} else {
+    console.log ('Число додатнє');
+}
+
+let word = 'war3432432';
+
+console.log('Кількість букв в змінній word: ' + word.length);
