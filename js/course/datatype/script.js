a15d7a1a9523facd7f45f73b2ddafaee2e9b7a5f
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-10-26 17:38:23 +0300

    Create script.js

---
+++ js/course/datatype/script.js
@@ -0,0 +1,38 @@
+// string (Рядок)
+const name = 'Dima'; 
+const surname = "Haborak";
+const surnamename = `Dmytrovuch`;
+
+// Інтерполяція рядків
+const message = `${name} ${surname}`
+
+console.log(message);
+
+// number (Число)
+const age = 28; 
+const posX = 50;
+const posY = -100;
+const posZ = 1.55; 
+
+const point = (posX + posY + posZ) * 2 / posX
+
+console.log(point);
+
+console.log('Word' / 100)
+
+console.log(100 / 0)
+
+console.log(-100 / 0)
+
+
+const isDeveloper = true; // boolean (Булеве значення)
+const power = 9999999; // number (Число)
+const sign = Symbol(); // symbol (Символ)
+const something = undefined; // undefined (Невизначений)
+
+const user = { // object (Об'єкт)
+    login: '123', // string (Рядок)
+    password: 'qwerty' // string (Рядок)
+};
+
+const data = null; // null (Порожнє значення / Нуль)
