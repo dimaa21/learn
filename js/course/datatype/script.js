c023ad9058fbf114d027048d8bf0408ebb806ff0
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-10-26 18:23:28 +0300

    Update script.js

--- js/course/datatype/script.js
+++ js/course/datatype/script.js
@@ -25,14 +25,55 @@ console.log(100 / 0)
 console.log(-100 / 0)
 
 
-const isDeveloper = true; // boolean (Булеве значення)
+// bigInt(Великі Числа)
+console.log(990000000009991n + 1n)
+console.log(990000000009992n + 2n)
+
+// boolean (Булеве значення)
+const ageChild = 8; 
+const isChild = ageChild < 18
+
+console.log(isChild);
+
+// null (Порожнє значення / Нуль)
+const data = null; 
+
+// object (Об'єкт)
+const user = { 
+    login: '123', 
+    password: 'qwerty'
+};
+
+const numbers = [4, 8, 15, 25, 50]
+const map = new Map()
+const set = new Set()
+const date = new Date()
+
+// typeof - перевірка типу даних 
+const Hello = 'Hi'
+
+console.log(
+    typeof Hello
+)
+
+// перетворення типів даних
+const num = 111
+const str = '222'
+const result = num + str
+
+console.log(result)
+
+
+//  перетворення типу string
+const MyAge = 24
+
+console.log(typeof MyAge)
+console.log(typeof String(MyAge))
+
 const power = 9999999; // number (Число)
 const sign = Symbol(); // symbol (Символ)
 const something = undefined; // undefined (Невизначений)
 
-const user = { // object (Об'єкт)
-    login: '123', // string (Рядок)
-    password: 'qwerty' // string (Рядок)
-};
 
-const data = null; // null (Порожнє значення / Нуль)
+
+
