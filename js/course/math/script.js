b5c014f2a3d89481ffbd3b5a00470f910279fc3f
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-11-03 22:41:34 +0200

    Update script.js

---
+++ js/course/math/script.js
@@ -0,0 +1,26 @@
+// додавання 
+console.log(2 + 2)
+
+// віднімання
+console.log(10 - 1)
+
+// множення 
+console.log(5 * 10)
+
+// ділення
+console.log(10 / 2)
+
+// множення степеню
+console.log(2 ** 10)
+
+// остача від ділення
+console.log(13 % 5)
+console.log(4 % 2)
+
+// унарний оператор 
+console.log(-10)
+
+// бінарний оператор
+console.log(2 + 2)
+
+console.log(5 * -2 + 10 / -3)
