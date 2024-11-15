5377c3e602c4d6fd7b881dff6c09c5068d0b8ddd
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-11-16 00:32:34 +0200

    Create index.js

---
+++ js/course/operator/index.js
@@ -0,0 +1,19 @@
+let myFirstVar = false;
+var mySecondVar = "Hello World";
+mySecondVar = "Hello";
+
+let num = 45;
+var nothing = undefined;
+
+let number = 47;
+var nothing = undefined;
+
+number = 45;
+
+let num_2 = 5;
+
+const Number = 34;
+
+let x = 56;
+let y = 2;
+let res;
