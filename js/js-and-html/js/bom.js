e257e3ddeefecb21586cd9b86a5c3eda50c44a5f
Dmytro Haborak <dimasukgaborak@gmail.com>
2024-11-16 10:03:45 +0200

    Update bom.js

--- js/js-and-html/js/bom.js
+++ js/js-and-html/js/bom.js
@@ -1,14 +1,18 @@
-/* console.log(window.innerHeight);
+// Об'єкти BOM 
 
-window.open('about:blank', 'hello', 'width:200, height=200');
+// window - це глобальний обʼєкт, який представляє вікно браузера
 
+// Виводимо поточну ширину екрану 
+console.log(window.innerWidth);
+
+// Виводимо поточну висоту екрану
+console.log(window.innerHeight);
+
+// Вспливаюче вікно
+// window.open('https://github.com/dimaa21');
+
+// Вспливаюче вікно з нашими параметрами 
+// window.open("about:blank", "hello", "width=200,height=200");
+
+// navigator — надає інформацію про браузер і операційну систему
 console.log(navigator.platform);
-
-console.log(location.href);
-
-setTimeout(function () {
-    location.reload();
-}, 2000);
-
-location.href = 'https://learn.javascript.ru/browser-environment';
-*/
