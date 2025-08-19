ba2da89f728ea05b1ea41c3782d735a3f805664b
Dmytro <haborakdmytro7@gmail.com>
2025-08-19 21:23:45 +0300

    Update script.js

--- js/course/сonditional-operators/script.js
+++ js/course/сonditional-operators/script.js
@@ -1,8 +1,10 @@
-var number = 15;
+var number = 20;
 
-// Оператор IF 
+// Оператор IF / ELSE IF / ELSE
 if(number == 14) {
-    console.log('Числа рівні');
+    console.log('Числа більше за 5');
+} else if(number > 10) {   // ✅ умова в дужках
+    console.log('Числа більше за 10');
 } else {
-    console.log('Числа нерівні');
+    console.log('Число більше за 15');
 }
