80ca3b35ad4742eb4ca48dc05e3d0379769687a9
Dmytro <haborakdmytro7@gmail.com>
2026-01-03 11:47:00 -0800

    commit

--- vue.js/main.js
+++ vue.js/main.js
@@ -1,7 +1,25 @@
-new Vue({
-  el: "#app",
-  data: {
-    title: "Hello World",
-    isTrue: true,
+const App = {
+  data() {
+    return {
+      placeholderString: 'Введіть назву замітки',
+      title: 'Список заміток',
+      inputValue: '',
+      notes: ['Замітка 1']
+    }
   },
-}); 
+
+  methods: {
+    inputChangeHandler(event) {
+      this.inputValue = event.target.value
+    },
+    addNewNote() {
+      this.notes.push(this.inputValue)
+      this.inputValue = ''
+    },
+    inputKeyPress(event) {
+      console.log(event.key)
+    }
+  }
+}
+
+Vue.createApp(App).mount('#app')
